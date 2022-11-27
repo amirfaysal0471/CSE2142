b95050f2c2198e64895a1bd2d6d1c9c7e2124ea8
Amir Faysal <amirfaysal0471@gmail.com>
2022-11-27 12:14:13 +0600

    remove temporary veriable

--- StudentList.java
+++ StudentList.java
@@ -67,7 +67,7 @@ public class StudentList {
 				}
 			}
 			} catch (Exception e){} 
-			System.out.println("Data Loaded.");				
+			System.out.println("Data Loaded.");	//co			
 		}
 		else if(args[0].contains("c")) 
 		{
